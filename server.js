c8bcbee5ae51d5c5167dc13da90e4b06f83b416b
Atmar Peeduli <kruthal@kruthal.com>
2021-01-22 12:55:58 +0200

    Commit

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const ejs = require('ejs');
+const bodyParser = require('body-parser');
+const mainPage = require('./routes/main');
+const errorPage = require('./routes/error')
+const app = express();
+
+app.set('view engine', ejs);
+app.use(express.static('public'));
+
+app.use(mainPage);
+app.use(errorPage);
+
+app.listen(3000, ()=> {
+    console.log('Server is running on port 3000.');
+});
